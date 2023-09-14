b998b037b2b9db4224aad6444261fff99bd78dc4
airwind69 <144618630+airwind69@users.noreply.github.com>
2023-09-14 11:01:34 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi ako si airwind madaliwats cashman aye</p>
+        <p>10 Things That Require Zero Talent</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
